6d7806e6d484562f1c323aa58ea62d0800820ceb
rednithin <nithin.r@instamojo.com>
2019-11-19 17:16:01 +0530

    Finished N Ary Tree PreOrder Traversal.

---
+++ NArayTreePreOrderTraversal.js
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/n-ary-tree-preorder-traversal/submissions/
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+/**
+ * @param {Node} root
+ * @return {number[]}
+ */
+var preorder = function(root) {
+  if(!root) {
+      return [];
+  }
+  return [
+      root.val,
+      ...root
+          .children
+          .filter(child => child)
+          .map(child => preorder(child))
+          .reduce((acc, curr) => [...acc, ...curr], [])
+  ]
+};
