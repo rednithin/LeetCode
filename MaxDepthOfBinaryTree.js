2507dc069f57a8c102647725ffc2d70d0fe14ae3
rednithin <nithin.r@instamojo.com>
2019-11-19 19:53:28 +0530

    Finished Max depth of binary tree.

---
+++ MaxDepthOfBinaryTree.js
@@ -0,0 +1,13 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const maxDepth = (root) => !root ? 0 : 1 +
+      Math.max(maxDepth(root.left), maxDepth(root.right))
