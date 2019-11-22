bfb3108ad38bc6039ee597b45f84ef4042957717
rednithin <nithin.r@instamojo.com>
2019-11-22 17:44:43 +0530

    Added alternate solution.

--- CousinsInBinaryTree.js
+++ CousinsInBinaryTree.js
@@ -40,3 +40,45 @@ var isCousins = function(root, x, y) {
   }
   return false;
 };
+
+
+/// Alternate solution to calculate depth - Using log2
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} x
+ * @param {number} y
+ * @return {boolean}
+ */
+var isCousins = function(root, x, y) {
+  let a = -1;
+  let b = -1;
+  
+  const inorder = (root, num = 1) => {
+    if(!root) {
+      return;
+    }
+    inorder(root.left, num * 2);
+    if(x == root.val) {
+      a = num;
+    }
+    if(y == root.val) {
+      b = num;
+    }
+    inorder(root.right,num * 2 + 1);
+  }
+  
+  inorder(root);
+  
+  if(parseInt(Math.log2(a)) == parseInt(Math.log2(b)) && parseInt(a / 2) != parseInt(b / 2)) {
+    return true;
+  }
+  return false;
+};
