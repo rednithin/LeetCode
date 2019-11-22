71975a73d5209d44e951f44912ae1ccf0c74d11a
rednithin <nithin.r@instamojo.com>
2019-11-22 17:41:03 +0530

    Cousins In Binary Tree.

---
+++ CousinsInBinaryTree.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} x
+ * @param {number} y
+ * @return {boolean}
+ */
+var isCousins = function(root, x, y) {
+  let a = -1;
+  let b = -1;
+  let d1 = -1;
+  let d2 = -1;
+  
+  const inorder = (root, depth = 1, num = 1) => {
+    if(!root) {
+      return;
+    }
+    inorder(root.left, depth+1, num * 2);
+    if(x == root.val) {
+      a = num;
+      d1 = depth;
+    }
+    if(y == root.val) {
+      b = num;
+      d2 = depth;
+    }
+    inorder(root.right, depth+1,num * 2 + 1);
+  }
+  
+  inorder(root);
+  console.log(a, b);
+  if(d1 == d2 && parseInt(a / 2) != parseInt(b / 2)) {
+    return true;
+  }
+  return false;
+};
