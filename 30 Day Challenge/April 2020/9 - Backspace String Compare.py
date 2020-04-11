862427233f706d07c60359f96d501323d3e8cbec
rednithin <reddy.nithinpg@live.com>
2020-04-11 20:48:30 +0530

    Added Elegant solution in comments.

--- 30 Day Challenge/April 2020/9 - Backspace String Compare.py
+++ 30 Day Challenge/April 2020/9 - Backspace String Compare.py
@@ -82,3 +82,19 @@ class Solution:
                 return False
             j -= 1
         return True
+
+
+# Elegant solution
+# class Solution(object):
+#     def backspaceCompare(self, S, T):
+#         def F(S):
+#             skip = 0
+#             for x in reversed(S):
+#                 if x == '#':
+#                     skip += 1
+#                 elif skip:
+#                     skip -= 1
+#                 else:
+#                     yield x
+
+#         return all(x == y for x, y in itertools.izip_longest(F(S), F(T)))
