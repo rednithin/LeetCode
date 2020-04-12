00598219d0e601403214fcfa6af486d3f6009a7c
rednithin <reddy.nithinpg@live.com>
2020-04-12 15:25:06 +0530

    Finished Rust solution.

---
+++ 30 Day Challenge/April 2020/12 - Last Stone Weight.rs
@@ -0,0 +1,18 @@
+use std::collections::BinaryHeap;
+
+impl Solution {
+    pub fn last_stone_weight(stones: Vec<i32>) -> i32 {
+        let mut heap = BinaryHeap::from(stones);
+
+        while heap.len() > 1 {
+            let a = heap.pop().unwrap();
+            let b = heap.pop().unwrap();
+            heap.push(a - b);
+        }
+
+        match heap.pop() {
+            Some(x) => x,
+            None => 0,
+        }
+    }
+}
