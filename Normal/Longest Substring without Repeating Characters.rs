838ef147b96429de647808f0c7fd0b15da45a98b
rednithin <reddy.nithinpg@live.com>
2020-05-08 13:39:12 +0530

    Finished Longest Substring without Repeating Characters in rust.

---
+++ Normal/Longest Substring without Repeating Characters.rs
@@ -0,0 +1,46 @@
+use std::collections::{
+    BTreeMap,   // Sorted Map
+    BTreeSet,   // Sorted Set
+    BinaryHeap, // Max Heap Implementation
+    HashMap,    // Map
+    HashSet,    // Set
+    LinkedList, // Linked List not to be used anytime
+    VecDeque,   // Deque
+};
+
+use std::cmp;
+
+impl Solution {
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        let mut longest = 0;
+        let mut i = 0;
+        let mut j = 0;
+        let mut set = HashSet::new();
+        let chars: Vec<char> = s.chars().into_iter().collect();
+        while j < chars.len() {
+            // println!("\n{} {}", i, j);
+            // println!("1: {:?}", set);
+            if i == j && i == 0 {
+                set.insert(chars[i]);
+                j += 1;
+                longest = cmp::max(longest, j - i);
+                continue;
+            }
+            if let Some(x) = set.get(&chars[j]) {
+                while chars[i] != chars[j] {
+                    // println!("----2: {:?}", set);
+                    // println!("----{} {}", chars[i], chars[j]);
+                    set.remove(&chars[i]);
+                    i += 1
+                }
+                i += 1;
+                j += 1;
+            } else {
+                set.insert(chars[j]);
+                j += 1;
+            }
+            longest = cmp::max(longest, j - i);
+        }
+        longest as i32
+    }
+}
