c08f2ee32df81deb612bac8ad759d43ea02e6399
rednithin <reddy.nithinpg@live.com>
2020-05-17 22:17:41 +0530

    Cleaned up solution.

--- Normal/Evaluate Reverse Polish Notiation.rs
+++ Normal/Evaluate Reverse Polish Notiation.rs
@@ -2,20 +2,20 @@ impl Solution {
     pub fn eval_rpn(tokens: Vec<String>) -> i32 {
         let mut stack = vec![];
         for token in tokens {
-            if ["+", "-", "/", "*"].iter().any(|&symbol| symbol == token) {
+            if let Ok(x) = token.parse::<i32>() {
+                stack.push(x);
+            } else {
                 let b = stack.pop().unwrap();
                 let a = stack.pop().unwrap();
                 let mut answer = 0;
-                match token.chars().nth(0).unwrap() {
-                    _ if "+" == token => answer = a + b,
-                    _ if "*" == token => answer = a * b,
-                    _ if "/" == token => answer = a / b,
-                    _ if "-" == token => answer = a - b,
+                match &token[..] {
+                    "+" => answer = a + b,
+                    "*" => answer = a * b,
+                    "/" => answer = a / b,
+                    "-" => answer = a - b,
                     _ => (),
                 }
                 stack.push(answer);
-            } else {
-                stack.push(token.parse::<i32>().unwrap());
             }
         }
         stack[0]
